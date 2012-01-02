c47b967f6b96646395238ba679b06c465000c2d5
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2012-01-01 19:18:13 -0500

    crediting youtube xbmc plugin for youtube url logic

--- resources/lib/vhx/scraper/youtube.py
+++ resources/lib/vhx/scraper/youtube.py
@@ -1,3 +1,24 @@
+'''
+   Copied the getVideoUrlMap() function from the xbmc youtube plugin,
+   so including their license.
+
+   YouTube plugin for XBMC
+   Copyright (C) 2010-2011 Tobias Ussing And Henrik Mosgaard Jensen
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import re
 from .common import *
 
@@ -99,6 +120,8 @@ class YouTubeVideo(object):
         return url
 
 def getVideoUrlMap(pl_obj, video = {}):
+    """ Borrowed from http://code.google.com/p/youtubexbmc/
+    """
 	links = {}
 	video["url_map"] = "true"
 				
